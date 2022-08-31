528726e0a66f9e2545e1e6bb79a21a0e2bf2c3ad
chenglijuan <18297788856@163.com>
2022-08-31 14:57:06 +0800

    提交

--- src/main/java/com/house/controller/UserController.java
+++ src/main/java/com/house/controller/UserController.java
@@ -1,33 +1,73 @@
 package com.house.controller;
 
+import com.house.entity.User;
+import com.mysql.cj.util.StringUtils;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
-import lombok.extern.log4j.Log4j;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.websocket.server.PathParam;
+import java.util.Objects;
 
 /**
  * @Data: 2022/8/31  10:47
  * @Decription:
  * @Modified:
  */
-@Log4j
+//swagger 配置
+//@EnableSwagger2
 @RestController
-@Api(value = "/user",hidden = true,description = "这是用户控制器")
+@Api(value = "/user",hidden = true,description = "用户控制器")
 @RequestMapping(value = "/user")
 public class UserController {
 
     @Autowired
     private SqlSessionTemplate template;
 
-    @RequestMapping(value = "getUserCount",method = RequestMethod.GET)
+    @RequestMapping(value = "/getUserCount",method = RequestMethod.GET)
     @ApiOperation(value = "/getUserCount",httpMethod = "GET")
     public int getUserCount(){
         int count  = template.selectOne("getUserCount");
         return count;
     }
+
+
+    @RequestMapping(value = "/addUser",method = RequestMethod.POST)
+    @ApiOperation(value = "/addUser",httpMethod = "POST")
+    public String addUser(@RequestBody User user){
+
+        if(Objects.isNull(user)){
+            return "参数不能为空";
+        }
+        if(StringUtils.isNullOrEmpty(user.getUserName())){
+            return "用户名不能为空";
+        }
+
+        int count  = template.insert("addUser",user);
+        return count+"";
+    }
+
+    /**
+     * 删除用户
+     * @return
+     */
+
+    @RequestMapping(value = "/deleteUser",method = RequestMethod.POST)
+    @ApiOperation(value = "/deleteUser",httpMethod = "POST")
+    public String deleteUser(@PathParam("userId") Integer userId){
+
+        if(Objects.isNull(userId)){
+            return "参数不能为空";
+        }
+        int count  = template.delete("deleteUser",userId);
+        return count+"";
+    }
+
+
 
 }
---
+++ src/main/java/com/house/entity/User.java
@@ -0,0 +1,22 @@
+package com.house.entity;
+
+import lombok.Data;
+
+/**
+ * @Author: chenglijuan
+ * @Data: 2022/8/31  13:33
+ * @Decription:
+ * @Modified:
+ */
+@Data
+public class User {
+
+    private String name;
+
+    private int age;
+
+    private String userName;
+
+    private String password;
+
+}
